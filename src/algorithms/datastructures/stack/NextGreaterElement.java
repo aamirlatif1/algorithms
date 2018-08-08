6ee89270d715e754c0c504cf9d2705c7f64d47a6
Aamir Latif <aamirlatif@gmail.com>
2018-08-08 12:41:44 +0200

    add initial structure

---
+++ src/algorithms/datastructures/stack/NextGreaterElement.java
@@ -0,0 +1,30 @@
+package algorithms.datastructures.stack;
+
+import java.util.Stack;
+
+/*
+https://www.cdn.geeksforgeeks.org/next-greater-element/
+ */
+public class NextGreaterElement {
+
+    static void nge(int[] array) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(array[0]);
+        int next = -1;
+        for (int i = 1; i < array.length; i++) {
+            next = array[i];
+            if (!stack.empty()) {
+                int element = stack.pop();
+                while (element < next) {
+                    System.out.printf("%d --> %d", element, next);
+
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 5, 2, 25};
+    }
+}
